662353a330d1f8505dc6f96cc4fa0e2bca172e0c
gdmannion <168197009+gdmannion@users.noreply.github.com>
2025-10-16 14:00:05 -0400

    Update main.js

--- bodywornliveselfhosted_golang-main/static/main.js
+++ bodywornliveselfhosted_golang-main/static/main.js
@@ -1,5 +1,7 @@
 import { startEventFeed } from './events.js';
+import { fetchToken } from './webrtc_video.js';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  await fetchToken();  // 🔁 Get fresh token on page load
   startEventFeed();
 });
